3aadded559c7a091b13cacbd112938580743a4e3
sinvv <sinvv2022@mail.ru>
2024-04-22 04:36:53 +0300

    second commit

---
+++ ver.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import sys
+
+print('Hello')
+def print_v():
+    print('Hello2')
+    print("Python version {}".format(sys.version))
+
+
+def main():
+	print_v()
+	
+if __name__=="__main__":
+    main()
